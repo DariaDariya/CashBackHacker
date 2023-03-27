8d3e2b82b104016881fdd3eea6aa90eb7221a631
Daria Test <dashik_gem@icloud.com>
2023-03-28 01:15:31 +0300

    Testng with second mistake commit

--- src/test/java/ru/netology/service/CashBackHackerTest.java
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -12,7 +12,31 @@ public class CashBackHackerTest {
         CashBackHacker service = new CashBackHacker();
         int amount = 900;
         int actual = service.remain(900);
-        int expected = 200;
+        int expected = 100;
         Assert.assertEquals(actual, expected);
         }
+    @org.testng.annotations.Test
+    public void shouldRemainAdd700IfAmount300() {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 300;
+        int actual = service.remain(300);
+        int expected = 700;
+        Assert.assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+    public void shouldRemainAdd1000IfAmount0() {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 0;
+        int actual = service.remain(0);
+        int expected = 1000;
+        Assert.assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+    public void shouldRemainAdd0IfAmount1000() {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        Assert.assertEquals(actual, expected);
+    }
     }
