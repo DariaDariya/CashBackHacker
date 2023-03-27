447a1cde3153945aef8eff96fa19a2a40310740e
Daria Test <dashik_gem@icloud.com>
2023-03-28 00:47:50 +0300

    New commit

---
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -0,0 +1,10 @@
+package ru.netology.service;
+
+import static org.testng.Assert.*;
+
+public class CashBackHackerTest {
+
+    @org.testng.annotations.Test
+    public void testRemain() {
+    }
+}
